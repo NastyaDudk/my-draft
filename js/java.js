a0d833dbe12abd8c084ec6a5f2707bcd204f8f3c
Anastasiia <144330798+NastyaDudk@users.noreply.github.com>
2023-12-28 20:40:13 +0400

    changed

---
+++ js/java.js
@@ -0,0 +1,73 @@
+// function showMessage(from, text) {
+//   if (text === undefined) {
+//     text = "text not found!";
+//   }
+
+//   console.log(from + ": " + text);
+// }
+
+// showMessage("Kate", "Hello");
+// showMessage("Daniel", "Oh. Hello, Kate!");
+// showMessage("Kate");
+
+// function sumNumbers(a, b) {
+//  return a + b;
+// }
+
+// let result = sumNumbers(1, 2);
+
+// console.log("file: java.js:19 > result=", result);
+
+// const username = "Nastya";
+
+// console.log(username);
+
+// const age = 27;
+
+// console.log(age);
+
+// const isStudent = true;
+
+// console.log(isStudent);
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers);
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// console.log(sum(70, 40));
+
+// function isEven(number) {
+//   if (number % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(isEven(8));
+// console.log(isEven(7));
+
+// function isEven(number) {
+//   return number % 2 === 0;
+// }
+
+// console.log(isEven(3));
+// console.log(isEven(7));
+
+// function getMax(numbers) {
+//   let max = numbers[0];
+//   for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > max) {
+//       max = numbers[i];
+//     }
+//   }
+//   return max;
+// }
+
+// console.log(getMax([3, 9, 2, 5, 8])); // Выведет 9
+// console.log(getMax([-1, -5, -2, -8])); // Выведет -1
+// console.log(getMax([10, 4, 6, 20])); // Выведет 20
